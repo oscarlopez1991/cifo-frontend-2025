ce413f84f0af7dc1607a7671ff7e58307eb1f52a
Oscar López Martínez <olm@MacBook-Air.local>
2025-07-11 17:10:39 +0200

    Programming challenge 2 resolution

--- reptes/repte2.js
+++ reptes/repte2.js
@@ -1,3 +1,18 @@
 // heu de crear una funció que rebi un únic paràmetre consistent en un array de números enters i retorni un altre array
 // de números enters que inclogui únicament els que són parells en el primer array.
 
+function filterArrayEvenNumbers(arrayNumbers) {
+    return arrayNumbers.filter((number) => number % 2 === 0)
+}
+
+function filterArrayEvenNumbersManually(arrayNumbers) {
+    const evenNumbers = []
+
+    for (let i = 0; i < arrayNumbers.length; i++) {
+        if (arrayNumbers[i] % 2 === 0) {
+            evenNumbers.push(arrayNumbers[i])
+        }
+    }
+
+    return evenNumbers
+}

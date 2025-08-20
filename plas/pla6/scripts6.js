24093df32d6bcaf018198911b5c4acb939871019
olm <oscar.bcn.1991@gmail.com>
2025-08-20 19:12:35 +0200

    Add initial implementation for PLA 6 project

---
+++ plas/pla6/scripts6.js
@@ -0,0 +1,97 @@
+/* Task 1 --------------------------------------------------------------------------------------- */
+
+/* This is a line with exactly 100 characters --------------------------------------------------- */
+
+/**
+ * The question about the DOM:
+ * Your answer here...
+ */
+
+/**
+ * The question about `defer`:
+ * Your answer here...
+ */
+
+/**
+ * The question about `getElementBy...` vs `getElementsBy...`:
+ * Your answer here...
+ */
+
+/* Task 2 --------------------------------------------------------------------------------------- */
+
+// There is no initial provided code.
+
+/* Task 2 solution ------------------------------------------------------------------------------ */
+
+/* Task 3 --------------------------------------------------------------------------------------- */
+
+let teams = [
+  {
+    team: 'South Korea',
+    games: {
+      wins: 0,
+      draws: 1,
+      losses: 2,
+    },
+  },
+  {
+    team: 'Denmark',
+    games: {
+      wins: 1,
+      draws: 0,
+      losses: 2,
+    },
+  },
+  {
+    team: 'Finland',
+    games: {
+      wins: 2,
+      draws: 1,
+      losses: 0,
+    },
+  },
+  {
+    team: 'Italy',
+    games: {
+      wins: 1,
+      draws: 2,
+      losses: 0,
+    },
+  },
+]
+
+/* Task 3 solution ------------------------------------------------------------------------------ */
+
+/* Task 4 --------------------------------------------------------------------------------------- */
+
+// There is no initial provided code.
+
+/* Task 4 solution ------------------------------------------------------------------------------ */
+
+/* Task 5 --------------------------------------------------------------------------------------- */
+
+let rating = 0
+
+/* Task 5 solution ------------------------------------------------------------------------------ */
+
+/* Task 6 --------------------------------------------------------------------------------------- */
+
+// This is how many pixels the bar has to move each time.
+const delta = 20
+// Same initial value as left: 200px in CSS.
+let left = 200
+
+/* Task 6 solution ------------------------------------------------------------------------------ */
+
+/* Task 7 --------------------------------------------------------------------------------------- */
+
+// These indicate how many pixels the ball has to move each time on each axis.
+let topDelta = (leftDelta = 5)
+// These are the coordinates for ball position.
+let topCoord = (leftCoord = 0)
+// This is the width of the playground area.
+const fieldWidth = document.querySelector('.ball-container').clientWidth
+// This is the height of the playground area.
+const fieldHeight = document.querySelector('.ball-container').clientHeight
+
+/* Task 7 solution ------------------------------------------------------------------------------ */

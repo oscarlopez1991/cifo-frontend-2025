715e6b163a50d30db11e38781d8724ee156c9bf5
olm <oscar.bcn.1991@gmail.com>
2025-09-23 16:56:22 +0200

    refactor: remove optional API key support from cryptoService

--- CryptoDemo/services/cryptoService.js
+++ CryptoDemo/services/cryptoService.js
@@ -1,14 +1,6 @@
 // Simple CoinGecko client for top market data
 const API_BASE = 'https://api.coingecko.com/api/v3';
 
-// Optional API key support (put your key in localStorage as "CG_API_KEY")
-const getHeaders = () => {
-  const key = localStorage.getItem('CG_API_KEY');
-  // Most public endpoints don’t need a key. If you have one, uncomment if your plan requires it:
-  // return key ? { 'x-cg-pro-api-key': key } : {};
-  return {};
-};
-
 /**
  * Fetch top coins by market cap.
  * @param {number} perPage - number of coins to fetch (max 250)
@@ -16,7 +8,7 @@ const getHeaders = () => {
  */
 export async function fetchTopMarkets(perPage = 100) {
   const url = `${API_BASE}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${perPage}&page=1&sparkline=false&price_change_percentage=24h`;
-  const res = await fetch(url, { headers: getHeaders() });
+  const res = await fetch(url);
   if (!res.ok)
     throw new Error(`CoinGecko error: ${res.status} ${res.statusText}`);
   const json = await res.json();

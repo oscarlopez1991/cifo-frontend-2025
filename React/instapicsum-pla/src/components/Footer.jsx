ca216a864dd4f743efb2a170c2c5dd6902682868
olm <oscar.bcn.1991@gmail.com>
2025-10-06 16:06:04 +0200

    feat: TODO 2 resolution

--- React/instapicsum-pla/src/components/Footer.jsx
+++ React/instapicsum-pla/src/components/Footer.jsx
@@ -2,16 +2,19 @@ const Footer = () => {
   return (
     // TODO #2
     // Podem eliminar l'etiqueta footer i simplement retornar els dos paràgrafs?
+    // Not directly because a React component must return a single root element.
     // Quina precaució hauríem de tenir si volguéssim fer-ho? (Pista: React.Fragment)
+    // --> We could use a React Fragment (`<>...</>`) to wrap the two paragraphs if we wanted to return them without an enclosing `<footer>` or another kind of root tag envolving them.
     <footer>
       <p>Instapicsum by CIFO L'Hospitalet</p>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero veritatis necessitatibus aut aliquam at,
-        exercitationem voluptas voluptates nihil doloremque deserunt, nesciunt eius expedita dolor quia pariatur fugit
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero veritatis
+        necessitatibus aut aliquam at, exercitationem voluptas voluptates nihil
+        doloremque deserunt, nesciunt eius expedita dolor quia pariatur fugit
         beatae? Necessitatibus, beatae.
       </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

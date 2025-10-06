ed0115cedae8c4a2391b2ad8d5f493eb5abcd066
olm <oscar.bcn.1991@gmail.com>
2025-10-06 21:05:36 +0200

    feat: TODOs Picture.jsx resolution

--- React/instapicsum-pla/src/components/Picture.jsx
+++ React/instapicsum-pla/src/components/Picture.jsx
@@ -1,13 +1,25 @@
 const Picture = ({ photo, author, timestamp }) => {
-  const photoTime = new Date(timestamp)
-  const today = new Date()
-  const diffTime = Math.abs(today - photoTime)
-  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24))
+  const photoTime = new Date(timestamp);
+  const today = new Date();
+  const diffTime = Math.abs(today - photoTime);
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+  // Helper for caption text
+  const getCaption = () => {
+    switch (diffDays) {
+      case 0:
+        return `Posted today by ${author}`;
+      case 1:
+        return `Posted yesterday by ${author}`;
+      default:
+        return `Posted ${diffDays} days ago by ${author}`;
+    }
+  };
 
   return (
     // TODO #11
     // Afegeix la classe de CSS picture on correspongui.
-    <div>
+    <div className="picture">
       <figure>
         <img src={photo} alt={`Uploaded by ${author}`} />
         <figcaption>
@@ -15,10 +27,11 @@ const Picture = ({ photo, author, timestamp }) => {
           /// Afegeix el contingut de figcaption perquè mostri el següent text:
           /// Posted [n] days ago by [nom autor fotografia]
           /// Pots fer que posi today i yesterday en comptes de "0 days ago" i "1 day ago"? */}
+          {getCaption()}
         </figcaption>
       </figure>
     </div>
-  )
-}
+  );
+};
 
-export default Picture
+export default Picture;

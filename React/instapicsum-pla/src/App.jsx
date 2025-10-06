a5456d95e53060359660f6629209babc048d92c9
olm <oscar.bcn.1991@gmail.com>
2025-10-06 18:56:58 +0200

    feat: TODOs App.jsx resolution

--- React/instapicsum-pla/src/App.jsx
+++ React/instapicsum-pla/src/App.jsx
@@ -1,25 +1,25 @@
-import { useState, useEffect } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import ListStories from './components/ListStories'
-import SuggestFollow from './components/SuggestFollow'
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ListStories from "./components/ListStories";
+import SuggestFollow from "./components/SuggestFollow";
 
 function App() {
-  const [suggestedFollows, setSuggestedFollows] = useState([])
-  const [stories, setStories] = useState([])
-  const [userLoggedIn, setUserLoggedIn] = useState('')
+  const [suggestedFollows, setSuggestedFollows] = useState([]);
+  const [stories, setStories] = useState([]);
+  const [userLoggedIn, setUserLoggedIn] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch('data.json')
-      const data = await res.json()
-      setSuggestedFollows(data.suggestedFollows)
-      setStories(data.stories)
-      setUserLoggedIn(data.userLoggedIn)
-    }
+      const res = await fetch("data.json");
+      const data = await res.json();
+      setSuggestedFollows(data.suggestedFollows);
+      setStories(data.stories);
+      setUserLoggedIn(data.userLoggedIn);
+    };
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   // Add comment.
   const addComment = (storyId, comment) => {
@@ -28,7 +28,25 @@ function App() {
     // Fixa't que rep un id d'story i un text amb el comentari a afegir.
     // Cal afegir aquest comentari (incorporant userLoggedIn com a usuari i la data actual) a la story en qüestió.
     // Després de fer-ho, React ha de repintar la story de manera automàtica perquè ja mostri el nou comentari.
-  }
+    setStories(
+      stories.map((story) =>
+        story.id === storyId
+          ? {
+              ...story,
+              comments: [
+                ...story.comments,
+                {
+                  id: crypto.randomUUID(),
+                  username: userLoggedIn,
+                  comment: comment,
+                  timestamp: new Date().toISOString(),
+                },
+              ],
+            }
+          : story
+      )
+    );
+  };
 
   return (
     <>
@@ -37,13 +55,19 @@ function App() {
       <SuggestFollow users={suggestedFollows} />
       <h2>Latest stories</h2>
 
-      {/* TODO #4
-      /// Afegeix aquí el component ListStories passant-li com a props l'array amb les stories i la funció per afegir un comentari. */}
+      {
+        /* TODO #4
+      /// Afegeix aquí el component ListStories passant-li com a props l'array amb les stories i la funció per afegir un comentari. */
+        <ListStories stories={stories} addComment={addComment} />
+      }
 
-      {/* TODO #5
-      /// Afegeix aquí el component Footer. */}
+      {
+        /* TODO #5
+      /// Afegeix aquí el component Footer. */
+        <Footer />
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;

ff3a67b696771fde6a33d99dce62156e96bcf870
olm <oscar.bcn.1991@gmail.com>
2025-10-06 20:58:30 +0200

    feat: TODO 10 resolution

--- React/instapicsum-pla/src/components/Header.jsx
+++ React/instapicsum-pla/src/components/Header.jsx
@@ -1,7 +1,11 @@
 const Header = () => {
   // TODO #10
   // Crea el JSX corresponent al component Header tenint en compte la semàntica de les etiquetes.
-  return {}
-}
+  return (
+    <header>
+      <h1>Instapicsum October 2025</h1>
+    </header>
+  );
+};
 
-export default Header
+export default Header;

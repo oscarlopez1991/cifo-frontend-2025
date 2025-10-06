410c399d55d3f5c5627d0fa83ce32ab79da92ac5
olm <oscar.bcn.1991@gmail.com>
2025-10-06 19:15:51 +0200

    feat: TODO 6 resolution

--- React/instapicsum-pla/src/components/Comment.jsx
+++ React/instapicsum-pla/src/components/Comment.jsx
@@ -3,10 +3,10 @@ const Comment = ({ author, text }) => {
     <div>
       {/* TODO #6
       /// Una de les dues línies següents presenta un problema evident que cal arreglar. */}
-      <p className='comment-author'>{author} said:</p>
-      <p className='comment-text'>text</p>
+      <p className="comment-author">{author} said:</p>
+      <p className="comment-text">{text}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Comment
+export default Comment;
